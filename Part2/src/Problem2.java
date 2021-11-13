7a70f2db05444d5c5ef3a11352d6efeb788b0081
felixwugithub <94074190+felixwugithub@users.noreply.github.com>
2021-11-12 21:40:41 -0800

    Completed task 1, 2, and 3

---
+++ Part2/src/Problem2.java
@@ -0,0 +1,15 @@
+public class Problem2 {
+
+    public static void main(String[] args) {
+        int a = 113;
+        double b = 2.71828;
+        String c = "Computer Science";
+
+        System.out.println("My grandpa is " + a + " years old.");
+        System.out.println(b + " is a random number. ");
+        System.out.println(c + " is the coolest field out there.");
+
+        
+
+    }
+}
